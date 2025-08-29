8f6fb6cfa28b3f88ee79c7c661dc0c38820a5216
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-08-28 22:51:49 -0400

    add

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,33 +1,50 @@
-import { Metadata } from "next";
+// app/not-found.tsx
 
-import "./globals.css";
-import NotFoundClient from "./not-found.client";
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import css from "./page.module.css"
+import { Metadata } from 'next';
+
 
 export const metadata: Metadata = {
-  title: "Not found page",
-  description: "Error 404",
+  title: "Not-Found Page",
+  description: "Something went wrong",
   openGraph: {
-    title: "Page not found",
-    description: "Sorry, the page you were looking for doesn't exist.",
-    url: "https://notehub.com/404",
-    siteName: "NoteHub",
-    images: [
-      {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Page not found illustration",
-      },
-    ],
-  },
+      title: "Not-Found Page",
+      description: "Something went wrong",
+      url: `https://notehub.com/not-found`,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt:  "Error",
+        },
+      ],
+      type: 'article',
+    },
+  
 };
-
 const NotFound = () => {
+  const router = useRouter();
+
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => router.push('/'), 3000);
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
-    <div className="error">
-      <NotFoundClient />
+    <div>
+      <h1 className={css.title}>404 - Page not found</h1>
+<p className={css.description}>Sorry, the page you are looking for does not exist.</p>
+
     </div>
   );
 };
 
-export default NotFound;
+export default NotFound
