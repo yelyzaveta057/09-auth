6b941053c176471f48ed7b009ab32a8e8c2c10ed
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-08-29 19:48:49 -0400

    aad

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiServer = axios.create({
-  baseURL: "https://notehub-api.goit.study",
+  baseURL: process.env.NEXT_PUBLIC_API_URL + '/api',
   withCredentials: true,
 });
